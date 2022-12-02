b8b6f125aa3985bf10f68a18d807222aac25b18d
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2022-12-02 15:41:37 +0100

    day2

---
+++ AOC-2022/day2/1/main.py
@@ -0,0 +1,26 @@
+with open("input.txt") as infile:
+    input_matches = infile.readlines()
+
+# example:
+# input_matches = ["A Y", "B X", "C Z"]  # score: 15
+
+# rock, paper scissors score table
+# score_table[opponent][me]
+#
+# example:
+# rock vs rock: score_table[0][0]  -> 4
+# paper vs scissors: score_table[1][2] -> 9
+score_table = [
+    [3+1, 6+2, 0+3],
+    [0+1, 3+2, 6+3],
+    [6+1, 0+2, 3+3]
+]
+
+score = 0
+
+for match in input_matches:
+    opponent = ["A", "B", "C"].index(match[0])
+    me = ["X", "Y", "Z"].index(match[2])
+    score += score_table[opponent][me]
+
+print(f"score: {score}")  # score: 13526
---
+++ AOC-2022/day2/2/main.py
@@ -0,0 +1,35 @@
+with open("input.txt") as infile:
+    input_matches = infile.readlines()
+
+# example:
+# input_matches = ["A Y", "B X", "C Z"]  # score: 15
+
+# rock, paper scissors score table
+# score_table[opponent][me]
+#
+# example:
+# rock vs rock: score_table[0][0]  -> 4
+# paper vs scissors: score_table[1][2] -> 9
+score_table = [
+    [3+1, 6+2, 0+3],
+    [0+1, 3+2, 6+3],
+    [6+1, 0+2, 3+3]
+]
+
+# score_table[instruction][opponent_shape]
+choose_shape_table = [
+    [2, 0, 1],
+    [0, 1, 2],
+    [1, 2, 0],
+]
+
+score = 0
+
+for match in input_matches:
+    opponent = ["A", "B", "C"].index(match[0])
+    instruction = ["X", "Y", "Z"].index(match[2])
+    me = choose_shape_table[instruction][opponent]
+
+    score += score_table[opponent][me]
+
+print(f"score: {score}")  # score: 14204
